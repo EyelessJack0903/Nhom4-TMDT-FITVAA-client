609a70568bb29d91634c83f4c633b8f256f49a99
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-11-11 21:51:16 +0700

    update code chien

---
+++ ecommerce-front/components/Featured.js
@@ -0,0 +1,71 @@
+import Center from "./Center";
+import styled from "styled-components";
+import Button from "./Button";
+import ButtonLink from "./ButtonLink";
+import CartIcon from "./icons/CartIcon";
+
+const Bg = styled.div`
+  background-color: #222;
+  color: #fff;
+  padding: 50px 0;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-weight: normal;
+  font-size: 3rem;
+`;
+
+const Desc = styled.p`
+  color: #aaa;
+  font-size: 0.8rem;
+`;
+
+const ColumnsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 40px;
+  
+  img {
+    max-width: 100%;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 25px;
+`;
+
+export default function Featured({product}) {
+  return (
+    <Bg>
+      <Center>
+        <ColumnsWrapper>
+          <Column>
+            <div>
+              <Title>{product.title}</Title>
+              <Desc>{product.description}</Desc>
+
+              <ButtonsWrapper>
+              <ButtonLink href={'/product/'+product._id} outline={1} white={1}>Read more</ButtonLink>
+                <Button white>
+                  <CartIcon />
+                  Add to cart
+                </Button>
+              </ButtonsWrapper>
+            </div>
+          </Column>
+          <Column>
+            <img src="https://nhom4-next-ecommerce.s3.amazonaws.com/1731252959604.png" alt="Product Image" />
+          </Column>
+        </ColumnsWrapper>
+      </Center>
+    </Bg>
+  );
+}
